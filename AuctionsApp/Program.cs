8729938febd09af64fd308ddb12134312411e7fd
Marius Kasparek <marius.kasparek@gmail.com>
2025-02-07 10:36:08 +0100

    Add Index page

--- AuctionsApp/Program.cs
+++ AuctionsApp/Program.cs
@@ -40,7 +40,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Listings}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
